d090346c97d4d9b6b6c035ba7f13e181376deab0
TheRABU <mdfazlerabbi114@gmail.com>
2025-10-07 22:39:57 +0600

    implemented the server.ts in a new way

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use("/api/v1", router);
 
 
-app.get("/", (req, res) => {
+app.get("/", (req:Request, res:Response) => {
   res.status(200).json({  
     message: "Server is live and running successfully",
   });
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,76 @@
 import app from "./app";
 import { connectDatabase } from "./db/connectDB";
 import { seedAdmin } from "./utils/seedAdmin";
+import { Server } from "http"
+
+let server: Server;
 
 const PORT = process.env.PORT || 5000;
 
 
 
-app.listen(PORT, () => {
-  console.log("Server has started and running sir!!");
 
-});
 
-const startServices = async () => { 
+const startServer = async () => { 
   try {
     await connectDatabase();
     await seedAdmin();
+      server = app.listen(PORT, () => {
+            console.log(`Server is listening to port ${PORT}`);
+        });
   } catch (error: any) {
     console.log(error.message);
   }
 }
 
-startServices();
+startServer();
+
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.log("SIGINT signal recieved... Server shutting down..");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+
+process.on("unhandledRejection", (err) => {
+    console.log("Unhandled Rejecttion detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("uncaughtException", (err) => {
+    console.log("Uncaught Exception detected... Server shutting down..", err);
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
